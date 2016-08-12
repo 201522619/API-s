a03257fdcc387fa92bd9c30d6cfad05aed41d183
felipe_miguel <felipe_miguel@Felipe-Miguel.datarey.info>
2016-08-12 13:44:59 -0300

    Commit

--- application-web_service/src/main/webapp/controller/appController.js
+++ application-web_service/src/main/webapp/controller/appController.js
@@ -1,9 +1,30 @@
 /**
  * Developer Felipe Miguel dos Santos
  */
-var app = angular.module('app', []);
+var app = angular.module('app', ['ngResource']);
 
-app.controller('appController', function (){
+app.factory('Conta', ['$resource', function ($resource){
+	return $resource('http://localhost:8080/conta',
+			{},//Parameters
+			{
+				update: {
+					method: 'PUT'
+				},
+				get: {
+					method: 'GET'
+				}
+			}
+	);
+}]);
+
+//https://www.sitepoint.com/creating-crud-app-minutes-angulars-resource/
+
+app.controller('appController', function (Conta){
 	var self = this;
 	self.title = "Controle pessoal";
+	self.contas = Conta.query(function(){
+		console.log(contas);
+	});
 });
+
+
